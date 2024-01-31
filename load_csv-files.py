3733ae91f1488208a194f1a4da61f21985432665
yashvichare <33318508+yashvichare@users.noreply.github.com>
2024-01-31 12:29:04 +0530

    first commi

---
+++ load_csv-files.py
@@ -0,0 +1,14 @@
+
+import pandas as pd
+import psycopg2
+from sqlalchemy import create_engine
+
+conn_string = 'postgresql://postgres:rootuser@localhost/painting'
+db = create_engine(conn_string)
+conn = db.connect()
+
+files = ['artist', 'canvas_size', 'image_link', 'museum_hours', 'museum', 'product_size', 'subject', 'work']
+
+for file in files:
+    df = pd.read_csv(f'/Users/yashvichare/Downloads/sqlcasestudy/famous paintings/{file}.csv')
+    df.to_sql(file, con=conn, if_exists='replace', index=False)
